499de4ee34b9083d8c32095769cc58cf32c121e0
Riyanbayu123 <riyanbayu0102@gmail.com>
2026-03-23 18:28:26 +0700

    enggk jd

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,11 +2,11 @@ import axios from 'axios'
 import router from '@/router'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://192.168.1.14:8000/api/v1',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
-    'ngrok-skip-browser-warning': 'true', // ← tambah ini
+    
   },
   timeout: 15000,
 })
